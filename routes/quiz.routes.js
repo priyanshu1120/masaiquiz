41960a8a0a402611b848fa2feab69386d466abba
priyanshu1120 <priyanshukaushik796@gmail.com>
2023-01-28 14:40:24 +0530

    edit in backend

--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -5,8 +5,7 @@ const { quizModel } = require("../model/quiz.model");
 let setcategory=[];
 
 quizRouter.get('/quiz',async (req,res)=>{
-    let {page=1,noofqus=10,category,difficulty} = req.query;
-
+    let {page=1,noofqus=10,category,difficulty} = req.query
     if(category && difficulty){
          let data = await quizModel.find({category,difficulty}).limit(noofqus).skip(noofqus*(page-1));
          return res.send(data);
